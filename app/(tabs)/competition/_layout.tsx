55fc047b7d9ad08f95981695f28ab31db8a563e6
torack51 <victor.aribaud@imt-atlantique.net>
2025-05-04 17:37:35 +0530

    Upgrade : début de la migration de react-native-snap-carousel vers react-native-reanimated-carousel, en cours, encore des ajustements à faire, notamment animation d'expansion et minimisation

--- app/(tabs)/competition/_layout.tsx
+++ app/(tabs)/competition/_layout.tsx
@@ -1,11 +1,26 @@
-import { Stack } from 'expo-router';
+import { router, Stack } from 'expo-router';
+import { Button } from 'react-native';
 
 export default function RootLayout() {
 
   return (
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="sportDetail/[sport_id]" options={({route}) => ({title : route.params?.name || 'Titre'})}/>
+        <Stack.Screen 
+          name="index" 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="sportDetail/[sport_id]" 
+          options={({ route }) => ({
+            title: route.params?.name || 'Titre',
+            headerLeft: () => (
+              <Button title="Retour" onPress={() => {
+                // Ici, tu définis l'action de retour que tu souhaites
+                router.back();
+              }} />
+            ),
+          })}
+        />
       </Stack>
   );
 }
