b114568ceeee3637927a8ae7860deb95f74cccaa
torack51 <victor.aribaud@imt-atlantique.net>
2025-06-16 18:28:23 +0200

    Ajout du sélecteur de sports dans les paramètres

--- app/(tabs)/other/index.tsx
+++ app/(tabs)/other/index.tsx
@@ -10,6 +10,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import ScreenLoader from '@/components/ScreenLoader';
 import { getUserByUid } from '@/backend/firestore/usersService';
 import { switchToAnonymousAfterLogout } from '@/backend/auth/authService';
+import SchoolPicker from '@/components/SchoolPicker';
 
 const width = Dimensions.get('window').width;
 
@@ -27,10 +28,16 @@ interface Team {
 }
 
 const ProfileScreen: React.FC = () => {
-    const [modalVisible, setModalVisible] = useState<boolean>(false);
+    const [schoolModalVisible, setSchoolModalVisible] = useState<boolean>(false);
+    const [sportsModalVisible, setSportsModalVisible] = useState<boolean>(false);
     const [announcements, setAnnouncements] = useState<Announcement[]>([]);
     const [loadingAnnouncements, setLoadingAnnouncements] = useState<boolean>(false);
     const [selectedTeam, setSelectedTeam] = useState<string | null>(null);
+    const [selectedTeamName, setSelectedTeamName] = useState<string | null>(null);
+    const [selectedTeamImage, setSelectedTeamImage] = useState<string | null>(null);
+    const [selectedTeamColor, setSelectedTeamColor] = useState<string | null>(null);
+    const [selectedSports, setSelectedSports] = useState<string[]>([]);
+    const [sports, setSports] = useState<string[]>([]);
     const [teams, setTeams] = useState<Team[]>([]);
     
     const [currentUser, setCurrentUser] = useState<any>(null);
@@ -149,11 +156,6 @@ const ProfileScreen: React.FC = () => {
                 <View style={styles.logo_container}> 
                     <Image source={require('../../../assets/images/logo-atlanticup-no-background.png')} style={{ width: width * 0.6, height: width * 0.5}} />
                 </View>
-                <View style={styles.description_container}>
-                    <Text>
-                        {"Cette application te permettra de suivre la progression du tournoi Atlanticup 2024 en temps réel! \nTu pourras aussi suivre les messages d'information donnés par les organisateurs."}
-                    </Text>
-                </View>
 
                 <View style={styles.annoucements_container}>
                     <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 20, color:'white' }}>
@@ -172,32 +174,48 @@ const ProfileScreen: React.FC = () => {
                     }
                 </View>
 
-                <View style={styles.team_selection_container}>
-                    <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold' }}>
-                        {"Mon équipe :"}
-                    </Text>
+                <View style={styles.selector_container}>
 
-                    <TouchableOpacity style={{ backgroundColor: '#4287f5', borderRadius: 20, padding: 10, margin: 10, justifyContent: 'center', alignItems: 'center' }} onPress={() => setModalVisible(true)}>
-                        {
-                            selectedTeamDetails != null ?
-                                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                                    <Image source={{ uri: selectedTeamDetails.image }} style={{ width: 100, height: 100 }} />
-                                    <Text style={{ textAlign: 'center', fontSize: 16, color: 'white', fontWeight: 'bold' }}>
-                                        {selectedTeamDetails.title}
-                                    </Text>
-                                </View>
-                                :
-                                <Text style={{ textAlign: 'center', fontSize: 16, color: 'white', fontWeight: 'bold' }}>
-                                    {"Sélectionner mon équipe"}
-                                </Text>
-                        }
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => { setSelectedTeam(null); AsyncStorage.removeItem("atlanticup_team") }} style={{ padding: 5, margin: 5, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text style={{ textAlign: 'center', fontSize: 12, color: 'grey', fontWeight: 'bold' }}>
-                            {"Réinitialiser mon équipe"}
+                    <View style={styles.team_selection_container}>
+                        <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold' }}>
+                            {"Mon équipe :"}
                         </Text>
-                    </TouchableOpacity>
+
+                        <TouchableOpacity style={{padding: 10, margin: 10, justifyContent: 'center', alignItems: 'center' }} onPress={() => setSchoolModalVisible(true)}>
+                            {
+                                selectedTeamDetails != null ?
+                                    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                        <Image source={{ uri: selectedTeamDetails.image }} style={{ width: 100, height: 100 }} />
+                                        <Text style={{ textAlign: 'center', fontSize: 16, color: 'white', fontWeight: 'bold' }}>
+                                            {selectedTeamDetails.title}
+                                        </Text>
+                                    </View>
+                                    :
+                                    <View style={{width:'100%', aspectRatio:1, justifyContent:'center', alignItems:'center', backgroundColor:'green'}}>
+                                        <SchoolPicker selectedSchoolID={selectedTeam} selectedSchoolName={selectedTeamDetails?.title} selectedSchoolImage={selectedTeamDetails?.image} selectedSchoolColor={selectedTeamDetails?.color} />
+                                    </View>
+                            }
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.sports_selection_container}>
+                        <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold' }}>
+                            {"Mes sports :"}
+                        </Text>
+
+                        <TouchableOpacity style={{ backgroundColor: '#4287f5', borderRadius: 20, padding: 10, margin: 10, justifyContent: 'center', alignItems: 'center' }} onPress={() => setSportsModalVisible(true)}>
+                            {
+
+                            }
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => { setSelectedSports([]); AsyncStorage.removeItem("atlanticup_sports") }} style={{ padding: 5, margin: 5, justifyContent: 'center', alignItems: 'center' }}>
+                            <Text style={{ textAlign: 'center', fontSize: 12, color: 'grey', fontWeight: 'bold' }}>
+                                {"Réinitialiser mes sports"}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
                 </View>
 
                 <View style={styles.legal_notices_container}>
@@ -226,10 +244,10 @@ const ProfileScreen: React.FC = () => {
             <Modal
                 animationType="fade"
                 transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => { setModalVisible(false) }}
+                visible={schoolModalVisible}
+                onRequestClose={() => { setSchoolModalVisible(false) }}
             >
-                <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+                <TouchableWithoutFeedback onPress={() => setSchoolModalVisible(false)}>
                     <View style={styles.modal_container}>
                         <View style={styles.modal_list_container}>
                             <FlatList
@@ -239,12 +257,16 @@ const ProfileScreen: React.FC = () => {
                                 contentContainerStyle={{ alignItems: 'center', justifyContent: 'center'}}
                                 showsVerticalScrollIndicator={false}
                                 renderItem={({ item }) => (
-                                    <TouchableOpacity onPress={() => { setSelectedTeam(item.id); setModalVisible(false); AsyncStorage.setItem("atlanticup_team", item.id); }} style={styles.team_card}>
+                                    <TouchableOpacity onPress={() => { setSelectedTeam(item.id); setSchoolModalVisible(false); AsyncStorage.setItem("atlanticup_team", item.id); }} style={styles.team_card}>
                                         <Image source={{ uri: item.image }} style={styles.team_card_image} />
                                         <Text style={styles.team_card_text}>{item.title}</Text>
                                     </TouchableOpacity>
                                 )}
+                                ListEmptyComponent={<Text>Aucune équipe trouvée</Text>}
                             />
+                                <TouchableOpacity onPress={() => { setSelectedTeam(null); setSchoolModalVisible(false); AsyncStorage.setItem("atlanticup_team", 'null'); }} style={{ margin: 10, padding: 10, backgroundColor: '#4287f5', borderRadius: 20 }}>
+                                    <Text style={{ color: 'white', fontWeight: 'bold' }}>Réinitialiser</Text>
+                                </TouchableOpacity>
                         </View>
                     </View>
                 </TouchableWithoutFeedback>
@@ -273,10 +295,21 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         padding: 10,
     },
+    selector_container: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
     team_selection_container: {
         alignItems: 'center',
         justifyContent: 'center',
-        marginBottom: 20,
+        flex:2,
+    },
+    sports_selection_container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex:3,
     },
     legal_notices_container: {
         alignItems: 'center',
