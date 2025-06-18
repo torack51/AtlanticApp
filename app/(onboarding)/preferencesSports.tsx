005c41c6a9dd73a1d6c17395212dcf2016a2af56
torack51 <victor.aribaud@imt-atlantique.net>
2025-06-18 15:35:22 +0200

    Upgrade: avancement dans la présentation du sélectionneur de sports à suivre dans l'onboarding, à suivre

--- app/(onboarding)/preferencesSports.tsx
+++ app/(onboarding)/preferencesSports.tsx
@@ -1,38 +1,83 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList} from 'react-native';
 import { Button } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { getAllSports } from '@/backend/firestore/sportsService';
 
 const PreferencesSports = () => {
 
     const router = useRouter();
-    const finishBoarding = async () => {
-        // Here you can save the user's preferences or perform any final actions
-        await AsyncStorage.setItem('hasSeenOnboarding', 'true');
-        router.replace('/(tabs)/calendar'); // Navigate to the home screen after finishing onboarding
+
+    const [sports, setSports] = React.useState<any[]>([]);
+    const [selectedSports, setSelectedSports] = React.useState<string[]>([]);
+    const [loading, setLoading] = React.useState(true);
+
+    const fetchSports = async () => {
+        try {
+            setLoading(true)
+            const sports = await getAllSports();
+            sports.map((sport: any) => {
+                console.log("Sport fetched:", sport);
+            });
+            setSports(sports);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error fetching sports:", error);
+            setLoading(false);
+        }
     };
+
+    const continueOnboarding = async () => {
+        // Here you can save the user's preferences or perform any final actions
+        router.push('/(onboarding)/allowNotifications'); // Navigate to the home screen after finishing onboarding
+    };
+
+    React.useEffect(() => {
+        fetchSports();
+    }, []);
+
+    console.log("Selected sports:", selectedSports);
 
     return (
         <SafeAreaView style={styles.main_container}>
-                    <View style={styles.title_container}>
-                        <Text style={styles.title}>Quels sports te font vibrer?</Text>
-                    </View>
-                    <View style={styles.team_selection_container}>
-                    
-                    </View>
-                    <View style={styles.buttons_container}>        
-                        <Button mode="contained" onPress={() => router.back()}>
-                            Revenir en arrière
-                        </Button>    
-                        <Button mode="contained" onPress={finishBoarding}>
-                            Terminer
-                        </Button>
-                    </View>
-        
-                    {/* Add your preferences UI components here */}
-                </SafeAreaView>
+            <View style={styles.title_container}>
+                <Text style={styles.title}>Quels sports te font vibrer?</Text>
+            </View>
+            <View style={styles.team_selection_container}>
+                <FlatList 
+                    data={sports}
+                    keyExtractor={(item) => item.id}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ alignItems:'center'}}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity 
+                            style={[styles.sport_item, selectedSports.includes(item.id) && { backgroundColor: 'green' }]} 
+                            onPress={() => {
+                                if (selectedSports.includes(item.id)) {
+                                    setSelectedSports(selectedSports.filter(id => id !== item.id));
+                                } else {
+                                    setSelectedSports([...selectedSports, item.id]);
+                                }
+                            }}
+                        >
+                            <Image source={{ uri: item.image }} style={styles.sport_image} />
+                            <Text style={styles.sport_title}>{item.title}</Text>
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
+            <View style={styles.buttons_container}>        
+                <Button mode="contained" onPress={() => router.back()}>
+                    Revenir en arrière
+                </Button>    
+                <Button mode="contained" onPress={continueOnboarding}>
+                    Terminer
+                </Button>
+            </View>
+
+        </SafeAreaView>
     );
 };
 
@@ -60,8 +105,35 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 20,
-        backgroundColor:'red',
-
+        paddingHorizontal: 40,
+    },
+    sports_container: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    sport_item: {
+        width: 250,
+        height: 75,
+        padding: 10,
+        marginHorizontal: 20,
+        marginVertical: 5,
+        borderRadius: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+    },
+    sport_image: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 10,
+    },
+    sport_title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333',
     },
     buttons_container: {
         flex: 1,
@@ -69,7 +141,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'row',
-        paddingHorizontal:20,
+        paddingHorizontal: 20,
     },
 });
 
