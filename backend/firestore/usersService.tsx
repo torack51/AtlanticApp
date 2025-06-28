e5c0ff3b5e44d9e385fbdd8edfe8e69036320d5d
torack51 <victor.aribaud@imt-atlantique.net>
2025-06-28 18:40:40 +0200

    Nouvelle fonction backend pour recevoir des notifications selon les préférences de l'utilisateur

--- backend/firestore/usersService.tsx
+++ backend/firestore/usersService.tsx
@@ -1,5 +1,5 @@
 import { collection, where, getDoc, doc, getDocs, setDoc, updateDoc, getFirestore, query} from '@react-native-firebase/firestore';
-
+import messaging from '@react-native-firebase/messaging';
 interface User {
     uid: string;
     anonymous: boolean;
@@ -60,6 +60,43 @@ export const updateUser = async (uid: string, data: Partial<User>): Promise<void
         await updateDoc(userRef, data);
     } catch (error) {
         console.error('Error updating user:', error);
+        throw error;
+    }
+}
+
+export const updateUserSupportedTeam = async (uid: string, teamId: string | null): Promise<void> => {
+    try {
+        const formerSupportedTeam = (await getUserByUid(uid))?.supported_team || null;
+        formerSupportedTeam ? await messaging().unsubscribeFromTopic(formerSupportedTeam) : null;
+        teamId ? await messaging().subscribeToTopic(teamId) : null;
+
+        const userRef = doc(db, 'users', uid);
+        await updateDoc(userRef, {supported_team: teamId});
+    } catch (error) {
+        console.error('Error updating user supported team:', error);
+        throw error;
+    }
+}
+
+export const updateUserFollowedSports = async (uid: string, sportsId : string[]): Promise<void> => {
+    try {
+        const formerFollowedSports = (await getUserByUid(uid))?.followed_sports || [];
+        formerFollowedSports.forEach(async (sportId) => {
+            if (sportId) {
+                await messaging().unsubscribeFromTopic(sportId);
+            }
+        });
+        sportsId.forEach(async (sportId) => {
+            if (sportId) {
+                console.log('Subscribing to topic:', sportId);
+                await messaging().subscribeToTopic(sportId);
+            }
+        });
+
+        const userRef = doc(db, 'users', uid);
+        await updateDoc(userRef, {followed_sports: sportsId});
+    } catch (error) {
+        console.error('Error updating user followed sports:', error);
         throw error;
     }
 }
